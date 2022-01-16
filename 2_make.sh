f5d626dcb3eddd1fb851c913237f6c99a8c3078b
Tamas Kenez <tamas.kenez@shapr3d.com>
2022-01-16 11:30:51 +0100

    fmt added

--- 2_make.sh
+++ 2_make.sh
@@ -1,20 +1,26 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+export CMAKE_BUILD_PARALLEL_LEVEL=1
+
 if false; then
 	cmake -Hd/JUCE -Bb/JUCE -GXcode -DCMAKE_INSTALL_PREFIX=$PWD/i \
 		-DJUCE_BUILD_EXAMPLES=0 -DJUCE_BUILD_EXTRAS=0 -DJUCE_ENABLE_MODULE_SOURCE_GROUPS=1	
-	cmake --build b/JUCE --config Debug --target install -j8
+	cmake --build b/JUCE --config Debug --target install
 fi
 
 cmake -Hd/gflags -Bb/gflags -DCMAKE_INSTALL_PREFIX=$PWD/i \
 	-DBUILD_STATIC_LIBS=1 \
 	-DBUILD_gflags_nothreads_LIB=0 \
 	-DREGISTER_INSTALL_PREFIX=0
-cmake --build b/gflags --config Release --target install -j8
+cmake --build b/gflags --config Release --target install
 
 cmake -Hd/glog -Bb/glog -DCMAKE_INSTALL_PREFIX=$PWD/i \
 	-DBUILD_SHARED_LIBS=0 \
 	-DWITH_GTEST=0 \
 	-DWITH_PKGCONFIG=0
-cmake --build b/glog --config Release --target install -j8
+cmake --build b/glog --config Release --target install
+
+cmake -Hd/fmt -Bb/fmt -DCMAKE_INSTALL_PREFIX=$PWD/i \
+	-DFMT_TEST=0 -DFMT_DOC=0
+cmake --build b/fmt --config Release --target install
